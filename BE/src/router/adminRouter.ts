37eb0118338138997bbb136541d8560db3090af5
quocm <quocminhhoang5004@gmail.com>
2023-06-13 16:52:54 +0700

    Added Comments and Modified Some Entity To Be UnNullable

--- BE/src/router/adminRouter.ts
+++ BE/src/router/adminRouter.ts
@@ -3,6 +3,7 @@ import AdminController from "../controller/adminController";
 
 const adminRouter = Router()
 
+//Router for Admin Specific Function
 adminRouter.post('/createAccount', AdminController.createAccount);
 
 export default adminRouter
--- BE/src/router/router.ts
+++ BE/src/router/router.ts
@@ -2,6 +2,8 @@ import {Router} from "express";
 import adminRouter from "./adminRouter";
 
 const router = Router();
+
+//Main Router 
 router.use('/admin', adminRouter)
 
 export default router;
