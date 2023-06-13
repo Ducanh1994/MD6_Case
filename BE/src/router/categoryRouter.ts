611d5b4b5f9468fd97504453012cbf1841c7dcbf
Ducanh1994 <121839766+Ducanh1994@users.noreply.github.com>
2023-06-13 10:09:08 +0700

    dev

--- BE/src/router/categoryRouter.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import categoryController from "../controller/categoryController";
-
-const categoryRouter = Router();
-
-categoryRouter.get('/', categoryController.findAllCategory)
-
-export default categoryRouter;
